7aca4ef9b3f009249bde0863d8c7d39c7f28b2f1
Bahnasy2001 <hassanbahnasy872@gmail.com>
2024-10-14 19:47:33 +0300

    Upload Pipeline

--- terraform/virtual_machine.tf
+++ terraform/virtual_machine.tf
@@ -25,22 +25,23 @@ resource "azurerm_public_ip" "public_ip" {
   sku                 = "Standard"  # Set SKU to Standard
 }
 resource "azurerm_network_security_group" "nsg" {
-  name                = "myNSG"
+  name                = "vm-nsg"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 
   security_rule {
-    name                       = "allow_ssh"
-    priority                   = 100
+    name                       = "AllowSSH"
+    priority                   = 1000
     direction                  = "Inbound"
     access                     = "Allow"
-    protocol                   = "Tcp"
+    protocol                   = "TCP"
     source_port_range          = "*"
-    destination_port_range     = 22  # SSH port
-    source_address_prefix      = "*"  # Allow from any IP (can be restricted for security)
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
 }
+
 resource "azurerm_network_interface" "nic" {
   name                = "myNIC"
   location            = azurerm_resource_group.rg.location
