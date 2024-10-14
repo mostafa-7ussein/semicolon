a0850975fce7f43a2d4d5c64ce1d03162adbbd26
Bahnasy2001 <hassanbahnasy872@gmail.com>
2024-10-14 19:04:46 +0300

    Upload Pipeline

---
+++ terraform/variables.tf
@@ -0,0 +1,9 @@
+locals {
+  resource_group = "semi-colon_aks_group"
+  location       = "UK South"
+}
+
+variable "client_id" {}
+variable "client_secret" {}
+variable "tenant_id" {}
+variable "subscription_id" {}
