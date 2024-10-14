f86ba2872a508f916dd2b5dcc5e6e418b85470e2
Bahnasy2001 <hassanbahnasy872@gmail.com>
2024-10-14 20:16:51 +0300

    Upload Pipeline

--- terraform/virtual_machine.tf
+++ terraform/virtual_machine.tf
@@ -34,13 +34,14 @@ resource "azurerm_network_security_group" "nsg" {
     priority                   = 1000
     direction                  = "Inbound"
     access                     = "Allow"
-    protocol                   = "TCP"
+    protocol                   = "Tcp"   # Corrected here
     source_port_range          = "*"
     destination_port_range     = "22"
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
 }
+
 
 resource "azurerm_network_interface" "nic" {
   name                = "myNIC"
