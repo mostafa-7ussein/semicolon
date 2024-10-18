2287bef4de0f5933b7bef01612a5e4559d8b3de6
mostafa-7ussein <mogahussein@gmail.com>
2024-10-18 12:48:57 +0300

    second

--- terraform/main.tf
+++ terraform/main.tf
@@ -77,7 +77,7 @@ resource "aws_security_group" "example_sg" {
 # Key Pair
 resource "aws_key_pair" "semicolon_key" {
   key_name   = "example-key"  # Name of the key pair
-  public_key = file("~/.ssh/id_rsa.pub")  # Path to your public key file
+  public_key = file("/home/mostafa/.ssh/id_rsa.pub")  # Path to your public key file
 }
 
 # 6. EC2 Instance Creation
