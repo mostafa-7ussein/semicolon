1a9c2d7e1544346229774b79975b13f0625b86a0
mostafa-7ussein <mogahussein@gmail.com>
2024-10-19 00:46:25 +0300

    update2000

--- terraform/main.tf
+++ terraform/main.tf
@@ -111,4 +111,5 @@ resource "aws_eip" "example_eip" {
 output "ec2_public_ip" {
   value       = aws_instance.example_ec2.public_ip
   description = "The public IP address of the EC2 instance."
-}
+}      
+
